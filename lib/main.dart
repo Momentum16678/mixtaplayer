146e35e81004e6463c7f4153c64aecfe41a271b6
Aveolart <aoladapo200@gmail.com>
2022-11-13 19:35:11 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:mixtaplayer/signin.dart';
 import 'package:mixtaplayer/signup.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get/get.dart';
 
 Future<void> main() async {
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
@@ -15,10 +16,25 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    //Because of the Getx dependency, we wil initialize it in MaterialApp to make it GetMaterialApp
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
 
@@ -26,19 +42,22 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   Future<void> startApp() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     var email = prefs.getString("email");
     print(email);
     Timer(
         const Duration(seconds: 3),
-            () => Navigator.pushReplacement(context,
-            MaterialPageRoute(builder: (context) => email == null ? const SignUpScreen() : const SignInScreen())));
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => email == null
+                    ? const SignUpScreen()
+                    : const SignInScreen())));
   }
 
   @override
@@ -53,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             gradient: LinearGradient(
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
